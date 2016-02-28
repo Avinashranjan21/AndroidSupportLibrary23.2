57b027bea98e203a81633c52cd89876d3e4785b7
Avinash Pandey <avinash.ranjan21@gmail.com>
2016-02-29 00:32:09 +0530

    Vector Drawable added to the imageView from java class

--- app/src/main/java/com/pandroid/androidsupportlibrary232/Vector_Drawable_Compat_Activity.java
+++ app/src/main/java/com/pandroid/androidsupportlibrary232/Vector_Drawable_Compat_Activity.java
@@ -2,6 +2,8 @@ package com.pandroid.androidsupportlibrary232;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Vector_Drawable_Compat_Activity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Vector_Drawable_Compat_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vector__drawable__compat_);
+
+        final ImageView mImageView = (ImageView) findViewById(R.id.iv_vectorDrawable);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mImageView.setImageResource(R.drawable.ic_radio);
+            }
+        });
     }
 }
