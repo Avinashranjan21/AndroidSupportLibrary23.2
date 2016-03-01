7a5eacb997283be90766250b57b5696d9eccdcf6
Avinash Pandey <avinash.ranjan21@gmail.com>
2016-03-02 01:31:03 +0530

    Runtime permission added

--- app/src/main/java/com/pandroid/androidsupportlibrary232/MainActivity.java
+++ app/src/main/java/com/pandroid/androidsupportlibrary232/MainActivity.java
@@ -2,16 +2,21 @@ package com.pandroid.androidsupportlibrary232;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button Vector_Drawable_Compat, AppCompat_DayNight_theme, Bottom_Sheets, MediaBrowserServiceCompat, Custom_Tabs;
+    Button Vector_Drawable_Compat, AppCompat_DayNight_theme, Bottom_Sheets, MediaBrowserServiceCompat, Custom_Tabs,app_permission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +27,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Bottom_Sheets = (Button) findViewById(R.id.Bottom_Sheets);
         MediaBrowserServiceCompat = (Button) findViewById(R.id.MediaBrowserServiceCompat);
         Custom_Tabs = (Button) findViewById(R.id.Custom_Tabs);
+        app_permission = (Button) findViewById(R.id.app_permission);
 
         Vector_Drawable_Compat.setOnClickListener(this);
         AppCompat_DayNight_theme.setOnClickListener(this);
         Bottom_Sheets.setOnClickListener(this);
         MediaBrowserServiceCompat.setOnClickListener(this);
         Custom_Tabs.setOnClickListener(this);
+        app_permission.setOnClickListener(this);
 
     }
 
@@ -35,6 +42,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         CustomTabsIntent mIntent = new CustomTabsIntent.Builder().build();
         String url = "http://www.Google.co.in";
         mIntent.launchUrl(this, Uri.parse(url));
+    }
+
+    private void checkPermissionRuntime(){
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)){
+
+                Toast.makeText(MainActivity.this, "I want to know where is my Girl Friend", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 
     @Override
@@ -52,6 +90,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.Custom_Tabs:
                 chromeCustomTab();
+                break;
+            case R.id.app_permission:
+                checkPermissionRuntime();
+
                 break;
         }
 
